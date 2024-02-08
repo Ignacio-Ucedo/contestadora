139ce69000728577c69ba47100aaab33f521b1c6
Ignacio-Ucedo <ig.ucedo@gmail.com>
2024-02-07 22:23:18 -0300

    7 feb 22:23

--- bot.py
+++ bot.py
@@ -9,8 +9,8 @@ class Bot:
         if not os.path.exists("mensajes_automaticos.xml"):
             root = ET.Element("raiz")
 
-            mensajes = ET.SubElement(root, "mensajes")
-            disparadores = ET.SubElement(root, "disparadores")
+            ET.SubElement(root, "contenidos")
+            ET.SubElement(root, "disparadores")
 
             tree = ET.ElementTree(root)
 
@@ -19,21 +19,30 @@ class Bot:
         else:
             tree = ET.parse("mensajes_automaticos.xml")
             root = tree.getroot()
-            print("se ha recuperado el archivo")
+            print("se ha recuperado el archivo de mensajes")
 
         if os.path.exists("interacciones.json"):
             with open("interacciones.json") as archivo:
-                contenido = archivo.read()
-                self.interacciones = json.load(contenido)
+                self.interacciones = json.load(archivo)
+        else:
+            self.interacciones = {}
+            self.actualizar_json()
 
-
-        self.root = root 
-        resultados = self.root.findall(".//mensaje")
+        self.mensajes = root
+        resultados = self.mensajes.findall(".//contenidos/mensaje")
         self.cantidad_mensajes = len(resultados)
 
-    def agregar_mensaje(self,contenido, lista_siguientes =None):
-        mensaje = ET.SubElement(self.root.find("mensajes"), "mensaje")
+        consulta_saludo = self.mensajes.find('.//disparador[@tipo="saludo"]/..')
+        if consulta_saludo != None:
+            self.saludo_id = consulta_saludo.get("id_mensaje")
+
+
+    def agregar_mensaje(self,contenido, id = None, lista_siguientes =None):
+        mensaje = ET.SubElement(self.mensajes.find("contenidos"), "mensaje")
         mensaje.set("id" , str(self.cantidad_mensajes))
+        if id:
+            resultado = self.mensajes.find(f'.//contenidos/mensaje[@id="{id}"]')
+            mensaje = resultado
         nodo_contenido = ET.SubElement(mensaje, "contenido")
         nodo_contenido.text = contenido
         self.cantidad_mensajes+=1
@@ -41,20 +50,54 @@ class Bot:
         if lista_siguientes != None:
             mensaje.set("lista_siguientes", str(lista_siguientes))
 
-        tree = ET.ElementTree(self.root)
+        tree = ET.ElementTree(self.mensajes)
         tree.write("mensajes_automaticos.xml")
     
-    def agregar_disparador(self, id_mensaje, tipo, entorno= None, contenido = None):
+    def agregar_disparador(self, id_mensaje, tipo, entorno= None, condicion = None, contenido = None):
         # Crear el elemento disparador
-        nodo_disparador = ET.SubElement(self.root.find("disparadores"), "disparador")
-        nodo_disparador.set("id_mensaje", str(id_mensaje))
+        resultado = self.mensajes.find(f'.//disparadores/mensaje[@id="{id_mensaje}"]')
+        if not resultado:
+            nodo_mensaje = ET.SubElement(self.mensajes.find("disparadores"), "mensaje")
+            nodo_mensaje.set("id_mensaje", str(id_mensaje))
+        else:
+            nodo_mensaje = resultado
+        nodo_disparador = ET.SubElement(nodo_mensaje, "disparador")
+
         nodo_disparador.set("tipo", tipo)
         if entorno :
            nodo_disparador.set("entorno", entorno)
 
+        if condicion:
+            nodo_disparador.set("condicion", condicion)
+
         if tipo == "texto":
             nodo_disparador.text = contenido
     
-        tree = ET.ElementTree(self.root)
+        tree = ET.ElementTree(self.mensajes)
         tree.write("mensajes_automaticos.xml")
 
+    #sirve para agregar también
+    def modificar_interaccion(self, usuario_id, ultimo_mensaje_id):
+        interaccion = self.interacciones.setdefault(usuario_id,{})
+        interaccion["ultimo_mensaje_id"] = ultimo_mensaje_id
+
+
+    def actualizar_json(self):
+        with open("interacciones.json", "w") as archivo:
+                json.dump(self.interacciones, archivo) 
+
+    def contestar(self, mensaje, usuario_id):
+        
+        if usuario_id not in self.interacciones:        
+            if self.saludo_id:
+                self.enviar_mensaje(self.saludo_id, usuario_id)
+                self.interacciones[usuario_id] = self.saludo_id
+
+
+    def enviar_mensaje(self, id_mensaje, usuario_id):
+        contenidos_a_enviar = self.mensajes.findall(f'.//mensaje[@id="{id_mensaje}"]/contenido')
+        for contenido in contenidos_a_enviar:
+            print(contenido.text)
+
+
+        
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import bot
 
 def main():
     b = bot.Bot()
-    # b.agregar_mensaje("hola")
+    b.agregar_mensaje("hola")
     b.agregar_disparador(0,"saludo")
-
+    b.agregar_mensaje("Este es un menu")
+    b.agregar_disparador(1, "texto", "local", "exacto", "hola")
+    b.agregar_disparador(1, "texto", "local", "incluido", "chau")
 
 main()
